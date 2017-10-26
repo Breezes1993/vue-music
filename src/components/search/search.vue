10b669b73e3d688d0faf6f3204c1c43b8d438fab
廖东阳 <413680568@qq.com>
2017-10-26 08:05:34 +0800

    Initial commit

---
+++ src/components/search/search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      搜索页面
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'search',
+  directives: {},
+  components: {},
+  data () {
+    return {
+    }
+  },
+  props: {},
+  computed: {},
+  watch: {},
+  methods: {},
+  beforeCreate () {},
+  created () {},
+  destroyed () {},
+  mounted () {}
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      歌手页面
+  </div>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  name: 'singer',
+  directives: {},
+  components: {},
+  data () {
+    return {
+    }
+  },
+  props: {},
+  computed: {},
+  watch: {},
+  methods: {},
+  beforeCreate () {},
+  created () {},
+  destroyed () {},
+  mounted () {}
+}
+</script>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
